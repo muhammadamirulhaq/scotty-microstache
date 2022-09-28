e72a72341392e249b18d7e48e67c9867cea586d4
Muhammad Amirul Haq <70356768+muhammadamirulhaq@users.noreply.github.com>
2022-09-28 08:24:19 +0800

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -6,9 +6,12 @@ import Web.Scotty
 import Data.Aeson ((.=), object)
 import Text.Microstache (compileMustacheFile, renderMustache)
 import Control.Monad.IO.Class (liftIO)
+import Data.Text
+
+name = "Mirhaq" :: Text
 
 main = scotty 3000 $ do
   get "/" $ do
     temp <- liftIO $ compileMustacheFile "index.html"
-    let val = object ["name" .= ("Mirhaq" :: String)]
+    let val = object [ "name" .= name ]
     html $ renderMustache temp val
